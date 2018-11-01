b717ea848ed5719a494149398dbb0fd33cb5f92e
Gautier Michelin <gm@ideesculture.com>
2018-11-01 19:22:08 +0100

    Ajout des noms

--- views/index_html.php
+++ views/index_html.php
@@ -18,7 +18,7 @@ define("__ASSET_IMAGE_URL__", __CA_URL_ROOT__."/app/plugins/suiviInventaireEglis
 
     <div class="row">
         <div class="col-md-6">
-            <h2>Tournai</h2>
+            <h2>Brabant-Wallon</h2>
             <div class="row">
                 <div class="col-md-6">
                     <div id="piscine" class="chart-container">
@@ -34,7 +34,41 @@ define("__ASSET_IMAGE_URL__", __CA_URL_ROOT__."/app/plugins/suiviInventaireEglis
         </div>
 
         <div class="col-md-6">
-            <h2>Tournai</h2>
+            <h2>Bruxelles</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <div id="bouboule" class="chart-container">
+                        <img src="<?php print __ASSET_IMAGE_URL__; ?>/pie.png"/>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div id="top_x_div2" class="chart-container">
+                        <img src="<?php print __ASSET_IMAGE_URL__; ?>/bars.png" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Liège</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <div id="piscine" class="chart-container">
+                        <img src="<?php print __ASSET_IMAGE_URL__; ?>/pie.png"/>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div id="top_x_div2" class="chart-container">
+                        <img src="<?php print __ASSET_IMAGE_URL__; ?>/bars.png" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <h2>Namur</h2>
             <div class="row">
                 <div class="col-md-6">
                     <div id="bouboule" class="chart-container">
@@ -66,26 +100,10 @@ define("__ASSET_IMAGE_URL__", __CA_URL_ROOT__."/app/plugins/suiviInventaireEglis
                 </div>
             </div>
         </div>
-
-        <div class="col-md-6">
-            <h2>Tournai</h2>
-            <div class="row">
-                <div class="col-md-6">
-                    <div id="bouboule" class="chart-container">
-                        <img src="<?php print __ASSET_IMAGE_URL__; ?>/pie.png"/>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div id="top_x_div2" class="chart-container">
-                        <img src="<?php print __ASSET_IMAGE_URL__; ?>/bars.png" />
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
-
 </div>
+<div style="margin-bottom:100px;clear:both;"></div>
 
 <style>
     .suiviInventaire > div,
